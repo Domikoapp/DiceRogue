f618c81a76745e19a524360ce51a0a5a7a687ee1
Domikoapp <domikoapp@gmail.com>
2016-05-11 04:58:02 +0900

    OS依存の改行文字に対応

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -3,41 +3,38 @@ using System.Collections;
 
 public class Map : MonoBehaviour
 {
-
 	public TextAsset mapText;
 	public GameObject p_nomalFloor;
 	public GameObject p_nomalWall;
 
-	// Use this for initialization
 	void Start ()
 	{
 		int row = 0;
 		int col = 0;
 		const int chipSize = 16;
-		foreach (char c in this.mapText.text) {
 
-			// when nextline appear, then go to nextline
-			if ((c == '\n') || (c == '\r')) {
-				row ++;
-				col = 0;
-				continue;
+		string lineSeparater = System.Environment.NewLine; // 改行文字を取得
+		string[] lines = mapText.text.Split (lineSeparater.ToCharArray (), System.StringSplitOptions.None);
+		foreach (string line in lines) {
+			foreach (char c in line) {
+				// マップチップを読み込みチェックする
+				GameObject chipObj = this.getChip (c);
+				if (chipObj == null) {
+					Debug.LogError ("Mapファイル内に想定外のな文字があります -> " + c);
+					continue;
+				}
+
+				// マップチップを配置する
+				chipObj.transform.position = new Vector2 (col * chipSize, -row * chipSize);
+				chipObj.transform.parent = this.transform;
+				col++;
 			}
-
-			// create new mapchip and check it
-			GameObject chipObj = this.getChip (c);
-			if (chipObj == null) {
-				Debug.LogError ("There is a illegal character in map data!");
-				continue;
-			}
-
-			// put mapchip into correct position
-			chipObj.transform.position = new Vector2 (col * chipSize, -row * chipSize);
-			chipObj.transform.parent = this.transform;
-			col ++;
+			// 1行出力し終えたら次の行へ
+			row++;
+			col = 0;
 		}
 	}
-	
-	// Update is called once per frame
+
 	private GameObject getChip (char c)
 	{
 		GameObject chipObj = null;
